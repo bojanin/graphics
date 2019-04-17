04dcc57a6061f77ac5913eae883b2185b258672b
ven1xus <bojanint@gmail.com>
2019-04-17 10:25:07 -0400

    lab 7 commit

---
+++ lab7/build/CMakeFiles/output.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/tommy/Documents/graphics/lab7/source/ClippingPlane.cpp" "/Users/tommy/Documents/graphics/lab7/build/CMakeFiles/output.dir/source/ClippingPlane.cpp.o"
+  "/Users/tommy/Documents/graphics/lab7/source/Defines.cpp" "/Users/tommy/Documents/graphics/lab7/build/CMakeFiles/output.dir/source/Defines.cpp.o"
+  "/Users/tommy/Documents/graphics/lab7/source/FrameBuffer.cpp" "/Users/tommy/Documents/graphics/lab7/build/CMakeFiles/output.dir/source/FrameBuffer.cpp.o"
+  "/Users/tommy/Documents/graphics/lab7/source/Lab.cpp" "/Users/tommy/Documents/graphics/lab7/build/CMakeFiles/output.dir/source/Lab.cpp.o"
+  "/Users/tommy/Documents/graphics/lab7/source/PerFragmentOperations.cpp" "/Users/tommy/Documents/graphics/lab7/build/CMakeFiles/output.dir/source/PerFragmentOperations.cpp.o"
+  "/Users/tommy/Documents/graphics/lab7/source/PerVertexOperations.cpp" "/Users/tommy/Documents/graphics/lab7/build/CMakeFiles/output.dir/source/PerVertexOperations.cpp.o"
+  "/Users/tommy/Documents/graphics/lab7/source/Rasterization.cpp" "/Users/tommy/Documents/graphics/lab7/build/CMakeFiles/output.dir/source/Rasterization.cpp.o"
+  "/Users/tommy/Documents/graphics/lab7/source/ShapeStructs.cpp" "/Users/tommy/Documents/graphics/lab7/build/CMakeFiles/output.dir/source/ShapeStructs.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/Users/tommy/Documents/graphics/lab7../glm"
+  "../source/headers"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
